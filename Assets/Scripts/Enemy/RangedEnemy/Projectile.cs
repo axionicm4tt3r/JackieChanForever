190f5dee9b1d95e0284dd4983bc813cb9f08d13d
AXiON <marcelroodt@gmail.com>
2017-12-04 23:09:24 +0200

    Playing Around With Stuff, Test ProBuilder

---
+++ Assets/Scripts/Enemy/RangedEnemy/Projectile.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+	public float fireballMaxVelocity = 15f;
+	public float fireballAcceleration = 12f;
+	Rigidbody fireballRigidbody;
+
+	// Use this for initialization
+	void Start () {
+		fireballRigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		fireballRigidbody.velocity += transform.forward * (fireballRigidbody.velocity.magnitude + (fireballAcceleration * Time.deltaTime));
+
+		if (fireballRigidbody.velocity.magnitude >= fireballMaxVelocity)
+		{
+			fireballRigidbody.velocity = fireballRigidbody.velocity.normalized * fireballMaxVelocity;
+		}
+	}
+}
