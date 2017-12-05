e3acac7d32276ddae05a6b828c1ab5faf5fe76ff
marcelroodt <marcelroodt@gmail.com>
2017-12-05 07:37:52 +0200

    Projectiles Work, Fixed Ranged AI NavMesh

--- Assets/Scripts/Enemy/RangedEnemy/Projectile.cs
+++ Assets/Scripts/Enemy/RangedEnemy/Projectile.cs
@@ -7,25 +7,25 @@ public class Projectile : MonoBehaviour {
 	public float fireballMaxVelocity = 15f;
 	public float fireballAcceleration = 12f;
 	Rigidbody fireballRigidbody;
-	GameObject player;
-
-	Vector3 targetVector;
 
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 		fireballRigidbody = GetComponent<Rigidbody>();
-		player = GameObject.FindGameObjectWithTag(Helpers.Tags.Player);
-
-		targetVector = (this.transform.position - player.transform.position);
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		fireballRigidbody.velocity += targetVector * (fireballRigidbody.velocity.magnitude + (fireballAcceleration * Time.deltaTime));
+		fireballRigidbody.velocity += transform.forward * (fireballRigidbody.velocity.magnitude + (fireballAcceleration * Time.deltaTime));
 
 		if (fireballRigidbody.velocity.magnitude >= fireballMaxVelocity)
 		{
 			fireballRigidbody.velocity = fireballRigidbody.velocity.normalized * fireballMaxVelocity;
 		}
+	}
+
+	void OnCollisionEnter()
+	{
+		Destroy(this.gameObject);
 	}
 }
