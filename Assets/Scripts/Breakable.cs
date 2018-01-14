02f76de8556d920577c6838c75e024be4597f590
Marcel Roodt <marcelroodt@gmail.com>
2018-01-14 19:33:37 +0200

    Throwable Damage based on Velocity
    Does more damage the higher the velocity is of the object. Target gets knocked back correctly

--- Assets/Scripts/Breakable.cs
+++ Assets/Scripts/Breakable.cs
@@ -1,18 +1,21 @@
 ﻿using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Collider))]
 public class Breakable : MonoBehaviour
 {
 	[ReadOnly]
 	public bool grabbed;
 
 	private Transform grabbedLocation;
-	private Rigidbody rigidBody;
+    private new Collider collider;
+	private new Rigidbody rigidbody;
 
 	public void Start()
 	{
 		grabbed = false;
-		rigidBody = GetComponent<Rigidbody>();
+        collider = GetComponent<Collider>();
+        rigidbody = GetComponent<Rigidbody>();
 	}
 
 	public void Update()
@@ -20,22 +23,49 @@ public class Breakable : MonoBehaviour
         if (grabbed)
         {
             transform.position = grabbedLocation.position;
-            //if ((transform.position - grabbedLocation.position).sqrMagnitude > .25f)
-            //    transform.position = Vector3.Lerp(transform.position, grabbedLocation.position, 0.1f);
         }
     }
 
 	public void Grab(Transform grabbedLocation)
 	{
 		grabbed = true;
-        rigidBody.isKinematic = true;
-		this.grabbedLocation = grabbedLocation;
+        rigidbody.isKinematic = true;
+        collider.enabled = false;
+        this.grabbedLocation = grabbedLocation;
 	}
 
     public void Drop()
     {
         grabbed = false;
-        rigidBody.isKinematic = false;
+        rigidbody.isKinematic = false;
+        collider.enabled = true;
         this.grabbedLocation = null;
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == Helpers.Tags.Enemy)
+        {
+            Debug.Log("Damaging Enemy with a Throwable!");
+            var currentVelocity = rigidbody.velocity.magnitude;
+
+            if (currentVelocity < 5f)
+            {
+                return;
+            }
+            else
+            {
+                var enemyAI = collision.gameObject.GetComponent<EnemyAI>();
+                var direction = (collision.gameObject.transform.position - transform.position).normalized;
+                enemyAI.ApplyKnockbackEffect(direction, 10f);
+                enemyAI.status.TakeDamage(PlayerController.JumpKickHitDamage);
+                return;
+            }
+        }
+
+        if (collision.gameObject.tag == Helpers.Tags.Player)
+        {
+            Debug.Log("Damaging Player with a Throwable!");
+        }
     }
 }
